2afb05d46812348d768e3a0484ac4849a431cd36
Onwuzulike Emeka <onwuzulikee1@gmail.com>
2016-10-02 21:49:49 +0100

    added admin

--- create_db.py
+++ create_db.py
@@ -2,6 +2,6 @@ from models import *
 
 db.create_all()
 
-user = User.query.filter_by(email='onwuzulikee1@gmail.com')
-db.session.delete(user)
+user = User('admin','admin@example.com','emmanuel11',True)
+db.session.add(user)
 db.session.commit()
